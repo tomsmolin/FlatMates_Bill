565a008f5415dfa92072e8d375a2c1630e4fe1c6
Tom Smolin <tomsmo016@gmail.com>
2021-12-05 19:23:35 +0200

    Adding design description file, and the basis of the objects classes

---
+++ main.py
@@ -0,0 +1,39 @@
+class Bill:
+    """
+    Object that contains data about a bill, such as total amount
+    and period of the bill
+    """
+
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
+
+
+class Flatmate:
+    """
+    Object that contains data about a flatmate, such as name, days in the house
+    the flatmate pays a share of the bill
+    """
+
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill):
+        pass
+
+
+class PdfReport:
+    """
+    Creates a PDF files that contains data about the flatmates such as
+    their names, their due amount and the period of the bill
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        pass
+
+
+
